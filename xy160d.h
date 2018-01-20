7a59d764c5cd7bd19a463fd6ef9540dc0bf80207
李辛迅 <meziden@hotmail.com>
2018-01-20 14:56:28 +0800

    Create xy160d.h

---
+++ xy160d.h
@@ -0,0 +1,26 @@
+#include "mbed.h"
+
+#ifndef XY160D_H
+#define XY160D_H
+
+const int XY160D_FORWARD    = 0x01;
+const int XY160D_BACKWARD   = 0x02;
+const int XY160D_HANG       = 0x01 | 0x02;
+const int XY160D_BREAK      = 0;
+
+class xy160d : public PwmOut
+{
+    public:
+        //constructors
+        xy160d(PinName pwmpin, PinName ctlpin1, PinName ctlpin2);
+        
+        void setmode(int mode_tmp);
+    
+    private:
+        DigitalOut ctl_1;
+        DigitalOut ctl_2;
+        
+        int mode_present;
+};
+
+#endif
