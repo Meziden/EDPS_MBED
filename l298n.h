6d549928f2ad43045331c4c8a10a3453edcb3d75
李辛迅 <meziden@hotmail.com>
2018-03-08 15:30:36 +0800

    Update l298n.h

--- l298n.h
+++ l298n.h
@@ -1,11 +1,11 @@
-#include "mbed.h"
-#include "rtos.h"
+//L298N Step Motor Drive
+
+#ifndef L298N_H
+#define L298N_H
 
 class l298n
 {
 public:
-    //在使用demux之后,l298n构造函数参数可以减至一个。
-    //因mbed现不支持c++11,构造函数不可委托，默认构造函数也不可为delete。
     l298n(PinName pin1,PinName pin2,PinName pin3,PinName pin4);
     
     //主线程更改l298n::stepcount并使用信号使rotate函数执行。
@@ -16,3 +16,5 @@ public:
 private:
     DigitalOut out1,out2,out3,out4;
 };
+
+#endif
