447d63c0cbaf29ec99881b4c7d741f3f0afdc812
李辛迅 <meziden@hotmail.com>
2018-03-31 19:41:46 +0800

    Update xy160d.cpp

--- xy160d.cpp
+++ xy160d.cpp
@@ -1,10 +1,11 @@
+#include "mbed.h"
 #include "xy160d.h"
 
 xy160d::xy160d(PinName pwmpin, PinName ctlpin1, PinName ctlpin2):PwmOut(pwmpin),ctl_1(ctlpin1),ctl_2(ctlpin2)
 {
     ctl_1 = 0;
     ctl_2 = 0;
-    mode_present = XY160D_BREAK;
+    mode_present = XY160D_BRAKE;
     period_ms(1);
     write(0.0f);
 }
@@ -24,13 +25,12 @@ void xy160d::setmode(int mode_tmp)
         case XY160D_FORWARD     : ctl_1 = 0; ctl_2 = 1; break;
         case XY160D_BACKWARD    : ctl_1 = 1; ctl_2 = 0; break;
         case XY160D_HANG        : ctl_1 = 1; ctl_2 = 1; break;
-        case XY160D_BREAK       : ctl_1 = 0; ctl_2 = 0; break;
+        case XY160D_BRAKE       : ctl_1 = 0; ctl_2 = 0; break;
         default:break;
     }
 }
 
-float xy160d::operator=(const float &value)
+void xy160d::operator=(const float &value)
 {
     write(value);
-    return value;
 }
