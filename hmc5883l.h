b2340871e97e984d317167508f9f054115847df5
李辛迅 <meziden@hotmail.com>
2018-04-11 09:32:40 +0800

    Update hmc5883l.h

--- hmc5883l.h
+++ hmc5883l.h
@@ -3,6 +3,23 @@
 #ifndef HMC5883L_H
 #define HMC5883L_H
 
+const float CALIB_OFFSET_X = 0.09;
+const float CALIB_OFFSET_Y = 0.00;
+const float CALIB_OFFSET_Z = 0.05;
+
+const float CALIB_SCALE_X = 1.00;
+const float CALIB_SCALE_Y = 1.00;
+const float CALIB_SCALE_Z = 0.97;
+
+const int GAIN_GAUSS_1370 = 0x00;
+const int GAIN_GAUSS_1090 = 0x02;   //Default Configuartion
+const int GAIN_GAUSS_820 = 0x04;
+const int GAIN_GAUSS_660 = 0x06;
+const int GAIN_GAUSS_440 = 0x08;
+const int GAIN_GAUSS_390 = 0x0A;
+const int GAIN_GAUSS_330 = 0x0C;
+const int GAIN_GAUSS_230 = 0x0E;
+
 class hmc5883l : public I2C
 {
     public:
@@ -20,6 +37,8 @@ class hmc5883l : public I2C
     void setscale(  float arg_x,
                     float arg_y,
                     float arg_z);
+                    
+    void setgain(int gainflag);
     
     private:
     float offset_x, offset_y, offset_z;
