adcc101aa1270e5ba514f458eff3f0792c87a8a5
李辛迅 <meziden@hotmail.com>
2018-04-22 21:34:16 +0800

    Update hmc5883l.h

--- hmc5883l.h
+++ hmc5883l.h
@@ -3,22 +3,26 @@
 #ifndef HMC5883L_H
 #define HMC5883L_H
 
-const float CALIB_OFFSET_X = 0.09;
-const float CALIB_OFFSET_Y = 0.00;
-const float CALIB_OFFSET_Z = 0.05;
+const float CALIB_OFFSET_X  = 0.09;
+const float CALIB_OFFSET_Y  = 0.00;
+const float CALIB_OFFSET_Z  = 0.05;
 
-const float CALIB_SCALE_X = 1.00;
-const float CALIB_SCALE_Y = 1.00;
-const float CALIB_SCALE_Z = 0.97;
+const int GAIN_GAUSS_1370   = 0x00;
+const int GAIN_GAUSS_1090   = 0x20;     //Default Configuartion
+const int GAIN_GAUSS_820    = 0x40;
+const int GAIN_GAUSS_660    = 0x60;
+const int GAIN_GAUSS_440    = 0x80;
+const int GAIN_GAUSS_390    = 0xA0;
+const int GAIN_GAUSS_330    = 0xC0;
+const int GAIN_GAUSS_230    = 0xE0;
 
-const int GAIN_GAUSS_1370 = 0x00;
-const int GAIN_GAUSS_1090 = 0x02;   //Default Configuartion
-const int GAIN_GAUSS_820 = 0x04;
-const int GAIN_GAUSS_660 = 0x06;
-const int GAIN_GAUSS_440 = 0x08;
-const int GAIN_GAUSS_390 = 0x0A;
-const int GAIN_GAUSS_330 = 0x0C;
-const int GAIN_GAUSS_230 = 0x0E;
+const int BIAS_NONE         = 0x00;     //Default Configuration
+const int BIAS_POSITIVE     = 0x01;
+const int BIAS_NEGATIVE     = 0x02;
+
+const int MODE_SINGLE       = 0x01;
+const int MODE_STREAM       = 0x00;     //Default Configuration
+const int MODE_HANG         = 0x03;
 
 class hmc5883l : public I2C
 {
@@ -26,24 +30,23 @@ class hmc5883l : public I2C
     //Constructor
     hmc5883l(PinName sda_pin, PinName scl_pin);
     
-    float getangle(int getcalibrated = 1);
+    float getangle();
     //debug only
     void getraw(uint8_t* bufferptr);
     
-    void setoffset( float arg_x,
-                    float arg_y,
-                    float arg_z);
-    
-    void setscale(  float arg_x,
-                    float arg_y,
-                    float arg_z);
-                    
+    //utils
     void setgain(int gainflag);
+    void setbias(int biasflag);
+    void setmode(int modeflag);
+    
+    //calibration related functions
+    void setoffset(float arg_x, float arg_y, float arg_z);
+    void setscale(float arg_x, float arg_y,float arg_z);
+    void calibscale();
     
     private:
     float offset_x, offset_y, offset_z;
     float scale_x, scale_y, scale_z;
-    
 };
 
 
