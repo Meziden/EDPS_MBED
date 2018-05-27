6d67673056d6e83a9b5178fb5420aabac30ee385
李辛迅 <meziden@hotmail.com>
2018-05-27 22:17:12 +0800

    Update hmc5883l.h

--- hmc5883l.h
+++ hmc5883l.h
@@ -3,26 +3,26 @@
 #ifndef HMC5883L_H
 #define HMC5883L_H
 
-const float CALIB_OFFSET_X  = 0.09;
-const float CALIB_OFFSET_Y  = 0.00;
-const float CALIB_OFFSET_Z  = 0.05;
+const float CALIB_OFFSET_X  = 10 / 2048.0;
+const float CALIB_OFFSET_Y  = 0 / 2048.0;
+const float CALIB_OFFSET_Z  = 25 / 2048.0;
 
-const int GAIN_GAUSS_1370   = 0x00;
-const int GAIN_GAUSS_1090   = 0x20;     //Default Configuartion
-const int GAIN_GAUSS_820    = 0x40;
-const int GAIN_GAUSS_660    = 0x60;
-const int GAIN_GAUSS_440    = 0x80;
-const int GAIN_GAUSS_390    = 0xA0;
-const int GAIN_GAUSS_330    = 0xC0;
-const int GAIN_GAUSS_230    = 0xE0;
+const uint8_t GAIN_GAUSS_1370   = 0x00;
+const uint8_t GAIN_GAUSS_1090   = 0x20;     //Default Configuartion
+const uint8_t GAIN_GAUSS_820    = 0x40;
+const uint8_t GAIN_GAUSS_660    = 0x60;
+const uint8_t GAIN_GAUSS_440    = 0x80;
+const uint8_t GAIN_GAUSS_390    = 0xA0;
+const uint8_t GAIN_GAUSS_330    = 0xC0;
+const uint8_t GAIN_GAUSS_230    = 0xE0;
 
-const int BIAS_NONE         = 0x00;     //Default Configuration
-const int BIAS_POSITIVE     = 0x01;
-const int BIAS_NEGATIVE     = 0x02;
+const uint8_t BIAS_NONE         = 0x00;     //Default Configuration
+const uint8_t BIAS_POSITIVE     = 0x01;
+const uint8_t BIAS_NEGATIVE     = 0x02;
 
-const int MODE_SINGLE       = 0x01;
-const int MODE_STREAM       = 0x00;     //Default Configuration
-const int MODE_HANG         = 0x03;
+const uint8_t MODE_SINGLE       = 0x01;     //Default Configuration
+const uint8_t MODE_STREAM       = 0x00;
+const uint8_t MODE_HANG         = 0x03;
 
 class hmc5883l : public I2C
 {
