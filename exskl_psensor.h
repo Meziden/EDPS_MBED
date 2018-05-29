603d93f85ba184e1fde1001bdb888a61f57078df
李辛迅 <meziden@hotmail.com>
2018-05-30 05:30:10 +0800

    Create exskl_psensor.h

---
+++ exskl_psensor.h
@@ -0,0 +1,47 @@
+//Data-only exskl_psensor encapsulation v2.0
+
+#ifndef EXSKL_PSENSOR_H
+#define EXSKL_PSENSOR_H
+
+const int BUFFER_SIZE = 20;
+const int CALIB_POSITION_MAX = 1;
+const int CALIB_POSITION_MIN = 2;
+
+const float SPEED_LIMIT = 0.8;
+const float CALIB_SPEED_LIMIT = 0.15;
+
+class exskl_psensor
+{
+    public:
+        //Constructor
+        exskl_psensor();
+        float getspeed();
+        
+        //Calibrate APIs
+        int calib_pressure();
+        int calib_position(int);
+        float getspeed_poscalib();
+        
+        //DEBUG APIs
+        float getvol(int);
+        float getposition();
+        
+        int write(  const float& vol1_tmp,
+                    const float& vol2_tmp,
+                    const float& position_tmp);
+        
+    private:
+        float position;
+        //store sample data
+        float buffer1[BUFFER_SIZE];
+        float buffer2[BUFFER_SIZE];
+        
+        //channel 1 and 2's offset.(Channel 1:bend,pos-,Channel 2:extend,pos+)
+        float offset1,offset2;
+        //channel 1 and 2's speed limit.
+        float speedlim1,speedlim2;
+        //channel 1 and 2's position limit.(poslim2 > poslim1)
+        float poslim1,poslim2;
+};
+
+#endif
