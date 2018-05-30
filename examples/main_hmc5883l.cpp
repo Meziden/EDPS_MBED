c8e3289db18b8bd9eb13e0adf9c2edce12c233d8
李辛迅 <meziden@hotmail.com>
2018-05-30 21:41:28 +0800

    Update main_hmc5883l.cpp

--- examples/main_hmc5883l.cpp
+++ examples/main_hmc5883l.cpp
@@ -6,13 +6,23 @@ Serial usbport(USBTX,USBRX);
 
 int main()
 {
+    uint8_t buffer[6] = {0};
     usbport.format(8, SerialBase::None, 1);
     usbport.baud(9600);
+    
+    compass.setgain(GAIN_GAUSS_1090);
+    compass.setmode(MODE_STREAM);
+    
     wait_ms(100);
     while(true)
     {
-        usbport.printf("ANGLE IS NOW %.3f\r\n",compass.getangle());
-        wait_ms(200);
+        compass.getraw(buffer);
+        //The output of following code can be  
+        usbport.printf("%d,%d,%d\n",(int16_t)((buffer[0]<<8)+buffer[1]),
+                                    (int16_t)((buffer[2]<<8)+buffer[3]),
+                                    (int16_t)((buffer[4]<<8)+buffer[5]));
+        //usbport.printf("ANGLE IS NOW %.3f\r\n",compass.getangle());
+        wait_ms(100);
     }
     return 0;
 }
