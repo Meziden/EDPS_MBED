b366bbc6d27035a55c8ae6f9ef905339136c937c
李辛迅 <meziden@hotmail.com>
2018-06-03 09:48:55 +0800

    Create tcs230.h

---
+++ tcs230.h
@@ -0,0 +1,53 @@
+#ifndef TCS230_H
+#define TCS230_H
+
+const float TCS230_TIMEOUT = 0.05;
+
+//DO NOT CHANGE THESE VALUE, OR INVALID ACCESS EXCEPTION HAPPENS
+const int RGB_CALIB[3]  = {2200,1900,2500};//for [1,1] freq configuration.
+const int COLOR_RED     = 0;
+const int COLOR_GREEN   = 1;
+const int COLOR_BLUE    = 2;
+
+//THESE CONSTS CAN BE CHANGED, SOME ARE HSV RELATED
+
+const int COLOR_HSV_RED = 0;
+const int COLOR_HSV_YELLOW = 1;
+const int COLOR_HSV_GREEN = 2;
+const int COLOR_HSV_CYAN = 3;
+const int COLOR_HSV_BLUE = 4;
+const int COLOR_HSV_MAGENTA = 5;
+
+class tcs230
+{
+    public:
+    //Constructor
+    tcs230( PinName fscale1_pin,
+            PinName fscale2_pin,
+            PinName diodectl1_pin,
+            PinName diodectl2_pin,
+            PinName recv_pin);
+    
+    int getcolor();
+    int getcolor_hsv();
+    int getraw(int* buffer);
+    
+    private:
+    Timeout timeout;
+    void callback_timeout();
+    
+    DigitalOut fscale1;
+    DigitalOut fscale2;
+    DigitalOut diodectl1;
+    DigitalOut diodectl2;
+    
+    InterruptIn receiver;
+    volatile int done;
+    volatile int sum;
+    bool changed;
+    void callback_rising();
+    
+    int rgb_buffer[3];
+};
+
+#endif
