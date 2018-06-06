cbcea0b4116bcd70c94c3602e2e0a6e05fdcd8bc
李辛迅 <meziden@hotmail.com>
2018-06-06 13:51:18 +0800

    Create imujy61.h

---
+++ imujy61.h
@@ -0,0 +1,24 @@
+#ifndef IMUJY61_H
+#define IMUJY61_H
+
+const size_t AXIS_X = 0;
+const size_t AXIS_Y = 1;
+const size_t AXIS_Z = 2;
+
+class imujy61 : public Serial
+{
+    public:
+    imujy61(PinName tx_pin, PinName rx_pin);
+    float getangle();
+    
+    uint8_t buffer[11];
+    
+    float accel[3];
+    float aspeed[3];
+    float angle[3];
+    
+    private:
+    void callback_rcvd();
+};
+
+#endif
