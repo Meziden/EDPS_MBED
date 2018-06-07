05bc60b114290f01a89abad07ec7d2b9dbd0903f
李辛迅 <meziden@hotmail.com>
2018-06-07 08:21:55 +0800

    11-byte aligning bug fixed.

--- imujy61.cpp
+++ imujy61.cpp
@@ -17,39 +17,50 @@ imujy61::imujy61(PinName tx_pin, PinName rx_pin) : Serial(tx_pin,rx_pin)
         accel[i] = 0;
     }
     
+    rcvd_count = 0;
+    packet_ready = 0;
+    
     //asynchronously - read buffer.
     attach(callback(this,&imujy61::callback_rcvd));
 }
 
 void imujy61::callback_rcvd()
 {
-    while(1)
-    {
-        read(buffer, 1);
-        if(buffer[0] != 0x55)
-            continue;
-        else
-            break;
-    }
-    read(buffer+1, 10);
+    buffer[rcvd_count] = getc();
+    if(rcvd_count == 0 && buffer[0] != 0x55)
+        return;
+    rcvd_count++;
     
-    switch(buffer[1])
+    if(rcvd_count == 11)
     {
-        case 0x51 :
-            accel[AXIS_X] = (short(buffer[3] << 8 | buffer[2])) / 32768.0 * 16;
-            accel[AXIS_Y] = (short(buffer[5] << 8 | buffer[4])) / 32768.0 * 16;
-            accel[AXIS_Z] = (short(buffer[7] << 8 | buffer[6])) / 32768.0 * 16;
-            break;
-        case 0x52 :
-            aspeed[AXIS_X] = (short(buffer[3] << 8 | buffer[2])) / 32768.0 * 2000;
-            aspeed[AXIS_Y] = (short(buffer[5] << 8 | buffer[4])) / 32768.0 * 2000;
-            aspeed[AXIS_Z] = (short(buffer[7] << 8 | buffer[6])) / 32768.0 * 2000;
-            break;
-        case 0x53 :
-            angle[AXIS_X] = (short(buffer[3] << 8 | buffer[2])) / 32768.0 * 180;
-            angle[AXIS_Y] = (short(buffer[5] << 8 | buffer[4])) / 32768.0 * 180;
-            angle[AXIS_Z] = (short(buffer[7] << 8 | buffer[6])) / 32768.0 * 180;
-            break;
+        packet_ready = 1;
+        rcvd_count = 0;
+    }
+    
+    if(packet_ready == 1)
+    {
+        packet_ready = 0;
+        if(buffer[0] == 0x55)
+        {
+            switch(buffer[1])
+            {
+                case 0x51 :
+                accel[AXIS_X] = (short(buffer[3] << 8 | buffer[2])) / 32768.0 * 16;
+                accel[AXIS_Y] = (short(buffer[5] << 8 | buffer[4])) / 32768.0 * 16;
+                accel[AXIS_Z] = (short(buffer[7] << 8 | buffer[6])) / 32768.0 * 16;
+                break;
+                case 0x52 :
+                aspeed[AXIS_X] = (short(buffer[3] << 8 | buffer[2])) / 32768.0 * 2000;
+                aspeed[AXIS_Y] = (short(buffer[5] << 8 | buffer[4])) / 32768.0 * 2000;
+                aspeed[AXIS_Z] = (short(buffer[7] << 8 | buffer[6])) / 32768.0 * 2000;
+                break;
+                case 0x53 :
+                angle[AXIS_X] = (short(buffer[3] << 8 | buffer[2])) / 32768.0 * 180;
+                angle[AXIS_Y] = (short(buffer[5] << 8 | buffer[4])) / 32768.0 * 180;
+                angle[AXIS_Z] = (short(buffer[7] << 8 | buffer[6])) / 32768.0 * 180;
+                break;
+            }
+        }
     }
 }
 
