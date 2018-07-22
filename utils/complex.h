7e5585460e3e9ed3bd8dc07fbb5efdff30f92248
李辛迅 <meziden@hotmail.com>
2018-07-22 15:38:09 +0800

    Create complex.h

---
+++ utils/complex.h
@@ -0,0 +1,37 @@
+// WARNING: This is only a simple, naive and uncompleted encapsulation for complex in ARM mbed.
+// If you got any problem, please email Zhang Siming(zhangsiming1724@hotmail.com) for more information.
+// Or issue at Meziden@GitHub(github.com/Meziden/mbed-libmezi).
+#ifndef COMPLEX_H
+#define COMPLEX_H
+
+class complex
+{
+    public:
+    // Default Constructor
+    complex();
+    // Another Constructor
+    complex(float arg_real, float arg_imag = 0.0f);
+    // Copy Constructor
+    complex(const complex &obj);
+    
+    // The most important data.
+    float real;
+    float imag;
+    
+    // Operator Overloads
+    complex& operator=(const complex &obj);
+    complex operator+(const complex &obj);
+    complex operator-(const complex &obj);
+    complex operator-();
+    
+    complex operator*(const complex &obj);
+    complex operator/(const complex &obj);
+    
+    // External functions
+    complex exp();
+    complex conj();
+    float abs();
+    float angle();
+};
+
+#endif
