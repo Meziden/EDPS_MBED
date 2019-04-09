439206872e8af315d1bfefc61ad3c0ece52d8a41
李辛迅 <meziden@hotmail.com>
2019-04-09 19:46:02 +0800

    Daemon is now supported.

--- spectrum/spectrum.h
+++ spectrum/spectrum.h
@@ -1,11 +1,17 @@
 #ifndef SPECTRUM_H
 #define SPECTRUM_H
 
+const int SUBNODE_MAX = 6;
+const int NODE_TYPE_SUBFNC = 0;
+const int NODE_TYPE_DAEMON = 1;
+const float DAEMON_PERIOD = 0.1;
+
+
 // Single SpectrumGL Node Definition
 class spectrum
 {
     public:
-    spectrum(const char* node_name, void (*node_func)(void));
+    spectrum(const char* node_name, void (*node_func)(void* arg) = NULL, void (*prev_func)(void* arg) = NULL, const int node_type = NODE_TYPE_SUBFNC);
     
     // Node Operations
     int add_node(spectrum* node_ptr);
@@ -13,11 +19,20 @@ class spectrum
     // Parent Node
     spectrum* m_parent;
     
-    spectrum* subnode_vec[5];
-    int subnode_num;
-    
+    // Node Name, Node Type and Function
     char m_name[16];
-    void (*m_func)(void);
+    int m_type;
+    void (*m_func)(void* arg);
+    
+    // if run as a Daemon.
+    Ticker m_ticker;
+    
+    // Data Preview Interface
+    void (*m_prev)(void* arg);
+    
+    // Subnode
+    spectrum* subnode_vec[SUBNODE_MAX];
+    int subnode_num;
 };
 
 #endif
