cf6d717bb53bb66aec54b1935769f695406bd3ec
李辛迅 <meziden@hotmail.com>
2019-04-13 21:05:25 +0800

    Multi argument and memory use optimizations.

--- cli_pseudo.cpp
+++ cli_pseudo.cpp
@@ -1,75 +1,101 @@
-//A simple command interpreter, only supports one command and one argument for each line.
-//It may support more arguments by using malloc, probably.
 #include "mbed.h"
 
-//Serial Port
-Serial usbport(USBTX,USBRX);
+/* Hardware Objects */ 
+Serial serial_port(D1, D0);
+//For NXP LPC1768， usethe following instead
+//Serial serial_port(USBTX, USBRX);
+DigitalOut myled(LED1);
 
-int scheduler(const char* cmd_tmp,const char* arg_tmp);
+/* Command Interpreter Parameters */
+const int MAX_ARGUMENT = 10;
+const int SCHEDULER_BUFSIZE = 320;  // 320bytes
+const bool SCHEDULER_ECHO = 1;
+
+void cli_callback(void);
+int scheduler(int argc, char** argv);
 
 int main()
 {
-    usbport.format(8,SerialBase::None,1);
-    usbport.baud(9600);
-    usbport.printf("\nStarting USB-UART port.\n");
+    /* Wake up every buddy. */
+    serial_port.baud(115200);
+    serial_port.format(8, SerialBase::None, 1);
+    serial_port.attach(&cli_callback, Serial::RxIrq);
     
-    // Command Interpreter
+    if(SCHEDULER_ECHO)
+        serial_port.printf("Serial @ STM32F303K8 > ");
+    
     while(1)
     {
-        char cmd_buf[32] = {'\0'};
-        char tmp_cmd[32]  = {'\0'};
-        char tmp_arg[32]  = {'\0'};
-        //read commands
-        int arg_flag = 0;
-        usbport.printf("usbuart @ lpc1768 > ");
-        usbport.gets(cmd_buf,32);
-        usbport.printf("%s",cmd_buf);
-        
-        //parsing input string
-        for(size_t i=0; i!=32; i++)
-        {
-            if(cmd_buf[i] == '\0')
-                break;
-            if(cmd_buf[i] == ' ' || cmd_buf[i] == '\n')
-                continue;
-            
-            if(cmd_buf[i] <= '9' && cmd_buf[i] >= '0' && (!arg_flag))  // is a number
-                arg_flag = i;                                          // also used as offset
-            
-            if(cmd_buf[i] == '-')
-                arg_flag = i;
-            
-            if(arg_flag)
-                tmp_arg[i - arg_flag] = cmd_buf[i];
-            else
-                tmp_cmd[i] = cmd_buf[i];
-        }
-        
-        if(scheduler(tmp_cmd,tmp_arg))
-            usbport.printf("[Interpreter] task complete.\n");
-        else
-            usbport.printf("[Interpreter] error:unexpected command\n");
+        myled = 1;
+        wait(1);
+        myled =0;
+        wait(1);
     }
-    return 0;
 }
 
-int scheduler(const char* cmd_tmp,const char* arg_tmp)
+/*
+ * Meziden's Simple Command Interpreter
+ * CLI Subsystem v2.0, comes with optimized memory use, multi-argument support and much, much more.
+ */
+
+void cli_callback(void)
+{
+    char cmd_buf[SCHEDULER_BUFSIZE]  = {'\0'};
+    
+    /* Get Command */
+    serial_port.gets(cmd_buf,SCHEDULER_BUFSIZE);
+    
+    if(SCHEDULER_ECHO)
+        serial_port.printf("%s",cmd_buf);
+    
+    /* Parsing input string. */
+    int cmd_argc = 1;
+    char* cmd_argv[MAX_ARGUMENT] = {cmd_buf};
+    int space_flag = 0;
+    /* Exit if reach the end of buffer */
+    for(size_t i = 0; i != SCHEDULER_BUFSIZE; i++)
+    {
+        /* Exit if end reached or argument number too large */
+        if(cmd_buf[i] == '\0' || cmd_buf[i] == '\n' || cmd_buf[i] == '\r')
+        {
+            cmd_buf[i] = '\0';
+        }
+        
+        if(cmd_argc == MAX_ARGUMENT)
+            break;
+        
+        if(cmd_buf[i] == ' ')
+        {
+            space_flag = 1;
+            cmd_buf[i] = '\0';
+            continue;
+        }
+
+        if(space_flag)
+        {
+            space_flag = 0;
+            cmd_argv[cmd_argc] = cmd_buf + i;
+            cmd_argc++;
+        }
+    }
+    
+    /* Exception Handling */
+    if(scheduler(cmd_argc,cmd_argv) < 0)
+        serial_port.printf("[CLI] error:unexpected command\n");
+    else
+        serial_port.printf("[CLI] task complete.\n");
+    
+    if(SCHEDULER_ECHO)
+        serial_port.printf("Serial @ STM32F303K8 > ");
+}
+
+int scheduler(int argc, char** argv)
 {
     //args and stats.
-    int argi_tmp = atoi(arg_tmp);
-    
-    //Add more if statements to support YOUR commands.
-    if(!strcmp("help",cmd_tmp))
+    if(!strcmp(argv[0],"hello"))
     {
-        usbport.printf("supported commands are:\nhelp version\n");
+        serial_port.printf("Hello!\n");
         return 1;
     }
-    
-    if(!strcmp("version",cmd_tmp))
-    {
-        usbport.printf("Simple CLI for ARM mbed v1.0\nBy the naive, for the naive.\n");
-        return 1;
-    }
-    
-    return 0;
+    return -1;
 }
